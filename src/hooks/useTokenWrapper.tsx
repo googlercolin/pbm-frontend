58ee02f5e4ece93cbed95988788f2f4b9d9ebeeb
Colin Hong <colin.hong18@gmail.com>
2023-10-11 21:56:37 +0800

    Added balanceOf from ABI

--- src/hooks/useTokenWrapper.tsx
+++ src/hooks/useTokenWrapper.tsx
@@ -1,15 +1,16 @@
 import { useEffect, useState, useCallback } from "react";
-import { Contract, ethers, Signer } from "ethers";
+import { Contract, ethers, Signer, TransactionResponse, TransactionReceipt } from "ethers";
 import { useWeb3 } from "./useWeb3";
 import TokenWrapperABI from "../ABIs/TokenWrapper.json";
 import { useGetPBMToken } from "./useGetPBMToken";
+
 
 // Define the contract parameters as a TypeScript interface
 interface ContractParams {
   signerOrProvider: Signer | ethers.BrowserProvider | null | undefined;
 }
 
-export function useTokenWrapperContract({ signerOrProvider }: ContractParams) {
+export function useTokenWrapperContract() {
   const [contract, setContract] = useState<Contract | null>(null);
   const { account, ethersProvider, signer } = useWeb3();
 
@@ -32,8 +33,8 @@ export function useTokenWrapperContract({ signerOrProvider }: ContractParams) {
       if (contract) {
         // run the code here
         try {
-          const txn = await contract.mint(account, id, amount, "");
-          await txn.wait();
+          const txn: TransactionResponse = await contract.mint(account, id, amount, "");
+          const receipt: TransactionReceipt | null = await txn.wait();
           return txn.hash;
         } catch (error) {
           console.log("error: ", error);
@@ -43,5 +44,19 @@ export function useTokenWrapperContract({ signerOrProvider }: ContractParams) {
     [contract]
   );
 
-  return { tokenWrapperContract: contract, mint };
+  const balanceOf = useCallback(
+    async (account: string, id: number) => {
+      if (contract) {
+        try {
+          const balance = await contract.balanceOf(account, id);
+          return balance;
+        } catch (error) {
+          console.log("error: ", error);
+        }
+      }
+    },
+    [contract]
+  );
+
+  return { tokenWrapperContract: contract, mint, balanceOf};
 }
