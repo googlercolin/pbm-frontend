8600b481a883ebecd744711c27051fa32d497e88
matthew-chua <mattchua2603@gmail.com>
2023-10-11 14:47:39 +0800

    added token wrapper and token manager abi:

--- src/hooks/useTokenWrapper.tsx
+++ src/hooks/useTokenWrapper.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState, useCallback } from "react";
 import { Contract, ethers, Signer } from "ethers";
 import { useWeb3 } from "./useWeb3";
+import TokenWrapperABI from "../ABIs/TokenWrapper.json";
+
 
 // Define the contract parameters as a TypeScript interface
 interface ContractParams {
@@ -11,8 +13,8 @@ export function useTokenWrapperContract({ signerOrProvider }: ContractParams) {
   const [contract, setContract] = useState<Contract | null>(null);
   const { account, ethersProvider, signer } = useWeb3();
 
-  const contractAddress = "0x708e827AB49aA06B3b541F42f80489CA0bDA9385";
-  const contractAbi = ""; // replace with the actual ABI
+  const contractAddress = "0xD13F9A5599Ba87DdeA389E3a6c9e1fC119f1634e";
+  const contractAbi = TokenWrapperABI; // replace with the actual ABI
 
   useEffect(() => {
     if (contractAddress && contractAbi && ethersProvider) {
