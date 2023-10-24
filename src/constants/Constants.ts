e6848adbbc3446c2d99d00dc3b4e04be4123004a
Ivan <ivanteozhiming@gmail.com>
2023-10-24 15:00:24 +0800

    work as on ivan computer

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,4 +1,4 @@
 export const ivansAddress = "0x319671626096aE7964d55DEa5c578840CFb15379";
 export const mattsAddress = "0x5432d22cE636A4fA6706FE28ac02A76b05ffec90";
 export const factoryAddress = "0x85401183a7f5dD20aB354d18AC20F57Da4C69208";
-export const merchantAddress = "0x7A08d18CBE0x319671626096aE7964d55DEa5c578840CFb15379547288738e72A5e209b402f7459c0e";
+export const merchantAddress = "0x319671626096aE7964d55DEa5c578840CFb15379";
